8844031bbc217ba756a448f1ca78ad32e33eeac6
Kota Miyake <miyake.kota@gmail.com>
2013-08-19 00:40:53 +0800

    create base template

---
+++ template.rb
@@ -0,0 +1,17 @@
+gem 'devise'
+gem 'haml-rails'
+
+gem_group :development do
+  gem 'rspec-rails'
+  gem 'guard-rspec'
+  gem 'quiet_assets'
+end
+
+gem_group :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'poltergeist'
+  gem 'factory_girl_rails'
+  gem 'spring'
+end
