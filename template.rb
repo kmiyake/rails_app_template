f53955bf4b5028e5730657460cbaaa7624b8b5c1
Kota Miyake <miyake.kota@gmail.com>
2013-09-08 23:47:36 +0800

    add guard-livereload

--- template.rb
+++ template.rb
@@ -4,6 +4,7 @@ gem 'haml-rails'
 gem_group :development do
   gem 'rspec-rails'
   gem 'guard-rspec'
+  gem 'guard-livereload'
   gem 'quiet_assets'
 end
 
@@ -21,3 +22,4 @@ run "mkdir spec/support"
 run "wget https://raw.github.com/kmiyake/rails_app_template/master/devise.rb -O spec/support/devise.rb"
 run "wget https://raw.github.com/kmiyake/rails_app_template/master/factory_girl.rb -O spec/support/factory_girl.rb"
 run "bundle exec guard init rspec"
+run "bundle exec guard init livereload"
