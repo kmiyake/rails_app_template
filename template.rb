4a060262855a1f868e558f3d1229b75bf81798bf
Kota Miyake <miyake.kota@gmail.com>
2014-08-25 00:01:23 +0800

    Update

--- template.rb
+++ template.rb
@@ -1,15 +1,23 @@
+add_source 'https://rails-assets.org'
+
 if yes?("Do you need devise?")
   gem 'devise'
   has_devise = true
 end
+
+# view
 gem 'haml-rails'
-gem 'font-awesome-rails' if yes?("Do you need to use fontawesome")
-if yes?("Do you need to use twitter bootstrap?")
-  gem 'therubyracer'
-  gem 'less-rails' #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
-  gem 'twitter-bootstrap-rails', git: 'git://github.com/seyhunak/twitter-bootstrap-rails.git', branch: 'bootstrap3'
-end
 gem 'simple_form'
+
+# asset
+if yes?("Do you need backbone?")
+  gem 'rails-assets-backbone'
+  gem 'rails-assets-marionette'
+end
+if yes?("Do you need bootstrap?")
+  gem 'rails-assets-bootstrap', '3.1.1'
+end
+gem 'rails-assets-fontawesome' if yes?("Do you need fontawesome?")
 
 gem_group :development do
   gem 'erb2haml'
@@ -38,11 +46,17 @@ end
 run "bundle install"
 run "bundle exec rake haml:replace_erbs"
 run "bundle exec rails g rspec:install"
+if has_devise
+  run "bundle exec rails g devise:install"
+end
 run "bundle exec guard init rspec"
 run "bundle exec guard init livereload"
 run "bundle exec spring binstub --all"
+
 run "mkdir spec/support"
-run "wget https://raw.github.com/kmiyake/rails_app_template/master/devise.rb -O spec/support/devise.rb" if has_devise
+if has_devise
+  run "wget https://raw.github.com/kmiyake/rails_app_template/master/devise.rb -O spec/support/devise.rb"
+end
 run "wget https://raw.github.com/kmiyake/rails_app_template/master/factory_girl.rb -O spec/support/factory_girl.rb"
 git :init
 run "cp config/database.yml config/database.yml.example"
